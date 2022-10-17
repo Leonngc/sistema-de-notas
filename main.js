7f5ce6acad2f5e3d5684d4c63b8ac0c8ce42fbce
Leonngc <nicolasleongoncalvescorrea@gmail.com>
2022-10-16 22:32:30 -0300

    Att

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 //alert("Olá usuário! Esse é o nosso sistema de notas, insira suas notas e mostraremos sua média e se você passou ou reprovou.")
-const formu = document.getElementById('formu').onsubmit()
-    
-formu.addEventListener('submit', (e) =>{
+
+const formu = document.getElementById('formu')
+const name = document.getElementById('nome').value
+const classroom = document.getElementById('turma').value
+const matters = document.getElementsByName('materia').value
+ let nota1 = document.getElementById('nota1')
+  let nota2 = document.getElementById('nota2')
+let nota3 = document.getElementById('nota3')
+let nota4 = document.getElementById('nota4')
+
+
+formu.addEventListener("submit", (e) => {
     e.preventDefault()
-    checkInputs()
+
+    validationNotes()
 })
-
-function checkInputs(){
-    let nome_do_aluno = document.querySelector('input#nome').value 
-    let turma = document.querySelector('input#turma').value 
-    let nota1 = document.querySelector('input#nota1').value 
-    let nota2 = document.querySelector('input#nota2').value 
-    let nota3 = document.querySelector('input#nota3').value 
-    let nota4 = document.querySelector('input#nota4').value 
-
-    if(nome_do_aluno.length == '2'){
-        alert("Preencha os dados completos")
-    }
+function validationNotes(){
+    let nota1Value = Number(nota1.value)
+    let nota2Value = Number(nota2.value)
+    let nota3Value = Number(nota3.value)
+    let nota4Value = Number(nota4.value)
+    //FAZER APARECER A MÉDIA E DIZER SE FOI APROVADO OU REPROVADO
 }
 
